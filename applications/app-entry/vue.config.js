e1595da7649b1dd2227f1dc2041f6356383db07d
rocco <rocco.mormont@gmail.com>
2019-11-29 22:32:35 +0800

    build: 更新依赖; 更新打包方式; 更新 lerna 配置

--- applications/app-entry/vue.config.js
+++ applications/app-entry/vue.config.js
@@ -1,12 +1,20 @@
-const webpack = require('webpack');
-const APP_NAME = require('./package.json').name;
-const PORT = require('./package.json').devPort;
-const PROXY = require('./config/proxy');
+const webpack = require('webpack')
+const APP_NAME = require('./package.json').name
+const PORT = require('./package.json').devPort
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const PROXY = {
+  '/app-typescript/': {
+    target: 'http://localhost:10241/'
+  },
+  '/app-javascript/': {
+    target: 'http://localhost:10242/'
+  }
+}
 
-log('APP_NAME: ', APP_NAME);
-log('NODE_ENV: ', NODE_ENV);
+const NODE_ENV = process.env.NODE_ENV || 'development'
+
+log('APP_NAME: ', APP_NAME)
+log('NODE_ENV: ', NODE_ENV)
 
 module.exports = {
   publicPath: './',
@@ -15,23 +23,22 @@ module.exports = {
 
   configureWebpack: {
     externals: {
-      vue: 'Vue',
-      'element-ui': 'ELEMENT',
+      vue: 'Vue'
     },
 
     plugins: [
       new webpack.DefinePlugin({
-        'process.env.VUE_APP_NAME': JSON.stringify(APP_NAME),
-      }),
-    ],
+        'process.env.VUE_APP_NAME': JSON.stringify(APP_NAME)
+      })
+    ]
   },
 
   devServer: {
     port: PORT,
-    proxy: PROXY,
-  },
-};
+    proxy: PROXY
+  }
+}
 
 function log(label, content, options) {
-  console.log('\x1b[1m%s\x1b[31m%s\x1b[0m', label, content);
+  console.log('\x1b[1m%s\x1b[31m%s\x1b[0m', label, content)
 }
